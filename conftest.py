543ccc62e9c6d679a57687ab2116b212cd7ad4ba
Başak Yılmaz <129073381+basakyilmaz1998@users.noreply.github.com>
2026-03-03 21:18:51 +0300

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+from base.database_controller import DataBaseController
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    result = outcome.get_result()
+
+    if result.when == 'call' or (result.when == 'setup' and result.failed):
+        case_name = result.nodeid
+        path = result.fspath
+
+        if result.failed:
+            stack_trace = result.longreprtext + "\n" + result.capstdout
+        else:
+            stack_trace = result.capstdout
+
+        status = result.outcome
+        duration = result.duration
+
+        DataBaseController.insert_data(status)
