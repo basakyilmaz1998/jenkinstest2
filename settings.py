137e79e791c6770e3fe7c0cc306e6a7c57f590c0
Başak Yılmaz <129073381+basakyilmaz1998@users.noreply.github.com>
2026-03-03 21:53:56 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 
 class SettingKeys:
-    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_HOST = os.getenv("DB_HOST", "mysql")
     DB_USER = os.getenv("DB_USER", "root")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "Sananabanana.1")
     DB_DATABASE = os.getenv("DB_DATABASE", "roadmapDB")
