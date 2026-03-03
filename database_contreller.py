0d640bb196b4dfc76fe8d087f1e340bddeffec3a
Başak Yılmaz <129073381+basakyilmaz1998@users.noreply.github.com>
2026-03-03 21:16:21 +0300

    Create database_contreller.py

---
+++ database_contreller.py
@@ -0,0 +1,31 @@
+import pymysql
+
+from base.utils.settings import SettingKeys, Settings
+
+
+def connect():
+    return pymysql.connect(
+        host=SettingKeys.DB_HOST,
+        user=SettingKeys.DB_USER,
+        password=SettingKeys.DB_PASSWORD,
+        database=SettingKeys.DB_DATABASE
+    )
+
+
+class Database:
+    def __init__(self):
+        self.settings = Settings()
+
+
+class DataBaseController(Database):
+    def __init__(self):
+        Database.__init__(self)
+
+    @staticmethod
+    def insert_data(data=""):
+        db = connect()
+        cursor = db.cursor()
+        query = "INSERT INTO bootcampDB.case_reports (case_status) VALUES ('{}')".format(data)
+        cursor.execute(query)
+        db.commit()
+        db.close()
