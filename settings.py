df1853e07233cb655c7f68ad0d99b4151d1fe915
Başak Yılmaz <129073381+basakyilmaz1998@users.noreply.github.com>
2026-03-03 22:56:31 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,15 +2,17 @@ import os
 
 
 class SettingKeys:
-    DB_HOST = os.getenv("DB_HOST", "mysql")
+    DB_HOST = os.getenv("DB_HOST", "host.docker.internal")  # host.docker.internal -> host makine
+    DB_PORT = int(os.getenv("DB_PORT", 3307))               # host'taki MySQL portu
     DB_USER = os.getenv("DB_USER", "root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "Sananabanana.1")
-    DB_DATABASE = os.getenv("DB_DATABASE", "roadmapDB")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
+    DB_DATABASE = os.getenv("DB_DATABASE", "bootcampDB")
 
 
 class Settings:
     def __init__(self):
         self.db_host = SettingKeys.DB_HOST
+        self.db_port = SettingKeys.DB_PORT
         self.db_user = SettingKeys.DB_USER
         self.db_password = SettingKeys.DB_PASSWORD
         self.db_database = SettingKeys.DB_DATABASE
